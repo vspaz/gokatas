0d07d1d63d450f5bacffd86570d118281b02757e
vspaz <vladimir.edelman@gmail.com>
2020-12-27 21:48:34 +0100

    add tests

---
+++ strings/counter_test.go
@@ -0,0 +1,43 @@
+package strings
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+import _ "github.com/stretchr/testify/assert"
+
+func TestGetFirstNonRepeatedCharacterWithOneNonRepeatedCharacter(t *testing.T) {
+	got := GetFirstNonRepeatedCharacter("aabbcdd")
+	expected := "c"
+	assert.Equal(t, got, expected, "expected '%v'; got '%v'", got, expected)
+}
+
+func TestGetFirstNonRepeatedCharacterWithMultipleNonRepeatedCharacters(t *testing.T) {
+	got := GetFirstNonRepeatedCharacter("aabbcddeff")
+	expected := "c"
+	assert.Equal(t, got, expected, "expected '%v'; got '%v'", got, expected)
+}
+
+func TestGetFirstNonRepeatedCharacterAllCharactersAreRepeataed(t *testing.T) {
+	got := GetFirstNonRepeatedCharacter("aabbccddeeff")
+	expected := ""
+	assert.Equal(t, got, expected, "expected '%v'; got '%v'", got, expected)
+}
+
+func TestGetFirstNonRepeatedCharacterEmptyString(t *testing.T) {
+	got := GetFirstNonRepeatedCharacter("")
+	expected := ""
+	assert.Equal(t, got, expected, "expected '%v'; got '%v'", got, expected)
+}
+
+func TestGetFirstNonRepeatedCharacterAllNonRepeatedCharacters(t *testing.T) {
+	got := GetFirstNonRepeatedCharacter("abcdef")
+	expected := "a"
+	assert.Equal(t, got, expected, "expected '%v'; got '%v'", got, expected)
+}
+
+func TestGetFirstNonRepeatedCharacterNonRepeatedCharacterComesLast(t *testing.T) {
+	got := GetFirstNonRepeatedCharacter("aabbccddeef")
+	expected := "f"
+	assert.Equal(t, got, expected, "expected '%v'; got '%v'", got, expected)
+}
