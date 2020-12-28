8d5cc6e4fb53225d4e6a3d4c4a7722791fe7901e
vspaz <vladimir.edelman@gmail.com>
2020-12-28 21:51:20 +0100

    add a basic test

--- strings/charcounter_test.go
+++ strings/charcounter_test.go
@@ -1,1 +1,10 @@
 package strings
+
+import "testing"
+import "reflect"
+
+func TestCountChars(t *testing.T) {
+	got := countChars("aabbcc")
+	expected := map[int32]int{97: 2, 98: 2, 99: 2}
+	reflect.DeepEqual(expected, got)
+}
