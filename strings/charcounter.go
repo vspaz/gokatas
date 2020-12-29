8d541ca9a8309202fb83d268e83f83af5413a3f9
vspaz <vladimir.edelman@gmail.com>
2020-12-29 20:04:53 +0100

    style fix

--- strings/charcounter.go
+++ strings/charcounter.go
@@ -2,7 +2,7 @@ package strings
 
 func countChars(text string) map[int32]int {
 	charToCount := map[int32]int{}
-	for _, char := range(text) {
+	for _, char := range text {
 		count, _ := charToCount[char]
 		charToCount[char] = count + 1
 	}
--- strings/charcounter_test.go
+++ strings/charcounter_test.go
@@ -9,12 +9,12 @@ func TestCountChars(t *testing.T) {
 	reflect.DeepEqual(expected, got)
 }
 func TestCountCharsEmptyString(t *testing.T) {
-    got := countChars("")
-    expected := map[int32]int{}
-    reflect.DeepEqual(expected, got)
+	got := countChars("")
+	expected := map[int32]int{}
+	reflect.DeepEqual(expected, got)
 }
 func TestCountCharsDifferentCount(t *testing.T) {
 	got := countChars("aabbcccdddeeeefffffg")
-	expected := map[int32]int{97:2, 98:3, 99:4, 100:5, 101:1}
+	expected := map[int32]int{97: 2, 98: 3, 99: 4, 100: 5, 101: 1}
 	reflect.DeepEqual(expected, got)
 }
