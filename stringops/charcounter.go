468e3f2949db52e66314c2cc2eb78243d96e93e4
vspaz <vladimir.edelman@gmail.com>
2021-01-08 21:40:43 +0100

    simplify charcounter method

--- stringops/charcounter.go
+++ stringops/charcounter.go
@@ -3,9 +3,7 @@ package stringops
 func countChars(text string) map[string]int {
 	charToCount := map[string]int{}
 	for _, char := range text {
-		letter := string(char)
-		count, _ := charToCount[letter]
-		charToCount[letter] = count + 1
+		charToCount[string(char)]++
 	}
 	return charToCount
 }
