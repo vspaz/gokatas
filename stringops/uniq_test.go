3faf53054d6094132c9e4992212689c0ea391279
vspaz <vladimir.edelman@gmail.com>
2021-01-10 21:24:41 +0100

    add TestUniqWithUnsortedElementsFail test

--- stringops/uniq_test.go
+++ stringops/uniq_test.go
@@ -9,9 +9,18 @@ import (
 )
 
 func TestUniqWithSortedElementsOK(t *testing.T) {
-	sortedElements := "1\n2\n3\n4\n5\n6"
+	sortedElements := "1\n2\n3\n4\n5\n6\n"
 	in := bufio.NewReader(strings.NewReader(sortedElements))
 	out := new(bytes.Buffer)
 	err := Uniq(in, out)
 	assert.Equal(t, nil, err, "failed")
+}
+
+
+func TestUniqWithUnsortedElementsFail(t *testing.T) {
+	unsortedElements := "1\n10\n\n2\n"
+	in := bufio.NewReader(strings.NewReader(unsortedElements))
+	out := new(bytes.Buffer)
+	err := Uniq(in, out)
+	assert.NotEqual(t, nil, err, "failed")
 }
