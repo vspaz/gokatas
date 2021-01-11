89fbff720ef9ba1418d365fb822119a791d30d68
vspaz <vladimir.edelman@gmail.com>
2021-01-11 12:41:07 +0100

    add enums

--- designpatterns/factory/carfactory.go
+++ designpatterns/factory/carfactory.go
@@ -2,14 +2,22 @@ package factory
 
 import "fmt"
 
+type cars struct {
+	AUDI string
+	BMW  string
+	FORD string
+}
+
+
 func getCar(car string) (iCar, error) {
+	carMakes := &cars{AUDI: "AUDI", BMW: "BMW", FORD: "FORD"}
 	switch car {
-	case "Ford":
-		return NewFord()
-	case "BMW":
-		return NewBMW()
-	case "AUDI":
+	case carMakes.AUDI:
 		return NewAUID()
+	case carMakes.BMW:
+		return NewBMW()
+	case carMakes.FORD:
+		return NewFord()
 	default:
 		return nil, fmt.Errorf("'%s' is undefined", car)
 	}
