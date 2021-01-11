6a4e5158ca4f82b1438387911da6e438333f1c58
vspaz <vladimir.edelman@gmail.com>
2021-01-11 13:12:12 +0100

    add a test

--- designpatterns/factory/factory_test.go
+++ designpatterns/factory/factory_test.go
@@ -1,2 +1,12 @@
 package factory
 
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewCarFactory(t *testing.T) {
+	carFactory := NewCarFactory()
+	ford, _ := carFactory.getCar("FORD")
+	assert.Equal(t, &FORD{car:car{make:"", speed:0}}, ford)
+}
