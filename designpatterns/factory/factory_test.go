6c6cb706b7e76bd141b257bc066f0acc7db62c9d
vspaz <vladimir.edelman@gmail.com>
2021-01-11 13:14:09 +0100

    small style fix

--- designpatterns/factory/factory_test.go
+++ designpatterns/factory/factory_test.go
@@ -8,12 +8,11 @@ import (
 func TestNewCarFactoryFordOK(t *testing.T) {
 	carFactory := NewCarFactory()
 	ford, _ := carFactory.getCar("FORD")
-	assert.Equal(t, &FORD{car:car{make:"", speed:0}}, ford)
+	assert.Equal(t, &FORD{car: car{make: "", speed: 0}}, ford)
 }
 
 func TestNewCarFactoryAudiOK(t *testing.T) {
 	carFactory := NewCarFactory()
 	audi, _ := carFactory.getCar("AUDI")
-	assert.Equal(t, &AUDI{car:car{make:"", speed:0}}, audi)
+	assert.Equal(t, &AUDI{car: car{make: "", speed: 0}}, audi)
 }
-
