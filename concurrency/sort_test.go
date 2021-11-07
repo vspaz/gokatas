438daf752de31f4b4ce1b4c1cdd60f9f5a1574f3
vspaz <vladimir.edelman@gmail.com>
2021-11-07 21:41:38 +0100

    add test

--- concurrency/sort_test.go
+++ concurrency/sort_test.go
@@ -1,1 +1,11 @@
 package concurrency
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMergeSort(t *testing.T) {
+	data := []int{10, 0, -1, 31, 150, -50, 80, 90, 300, 1000, -180}
+	assert.Equal(t, []int{-180, -50, -1, 0, 10, 31, 80, 90, 150, 300, 1000}, MergeSort(data))
+}
