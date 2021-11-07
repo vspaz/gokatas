8422fb59ee097147993b0bccf6ce09cefa1b0037
vspaz <vladimir.edelman@gmail.com>
2021-11-07 21:08:34 +0100

    add 'Merge' function

--- concurrency/parallel_merge_sort.go
+++ concurrency/parallel_merge_sort.go
@@ -1,1 +1,19 @@
 package concurrency
+
+func Merge(left []int, right []int) []int {
+	merged := make([]int, 0, len(left)+len(right))
+	for len(left) > 0 || len(right) > 0 {
+		if len(left) == 0 {
+			return append(merged, right...)
+		} else if len(right) == 0 {
+			return append(merged, left...)
+		} else if left[0] < right[0] {
+			merged = append(merged, left[0])
+			left = left[1:]
+		} else {
+			merged = append(merged, right[0])
+			right = right[1:]
+		}
+	}
+	return merged
+}
