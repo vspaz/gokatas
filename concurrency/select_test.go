ef5da48465a3f8fe3c8a1a724727a56330c48930
vspaz <vladimir.edelman@gmail.com>
2021-11-09 14:03:25 +0100

    make coroutine wait on another

--- concurrency/select_test.go
+++ concurrency/select_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func worker(id string, channel chan string, waitInterval time.Duration) {
+func worker(id int, channel chan string, waitInterval time.Duration) {
 	for i := 0; i < 5; i++ {
 		channel <- fmt.Sprintf("worker %d; message '%d'", id, i)
 		time.Sleep(waitInterval)
@@ -18,21 +18,11 @@ func TestSelect(t *testing.T) {
 	channel_1 := make(chan string)
 	channel_2 := make(chan string)
 
-	go func() {
-		for i := 0; i < 5; i++ {
-			channel_1 <- fmt.Sprintf("worker 1; message '%d'", i)
-			time.Sleep(time.Millisecond * 100)
-		}
-	}()
-
-	go func() {
-		for i := 0; i < 5; i++ {
-			channel_2 <- fmt.Sprintf("worker 2; message '%d'", i)
-		}
-	}()
+	go worker(1, channel_1, time.Millisecond*100)
+	go worker(2, channel_2, time.Second*5)
 
 	for i := 0; i < 5; i++ {
 		assert.Equal(t, fmt.Sprintf("worker 1; message '%d'", i), <-channel_1)
-		assert.Equal(t, fmt.Sprintf("worker 2; message '%d'", i), <-channel_2)
+		assert.Equal(t, fmt.Sprintf("worker 2; message '%d'", i), <-channel_2)  // blocking
 	}
 }
