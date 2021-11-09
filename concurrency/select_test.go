a5abad41fa9cc489d34ebecb19ba078bfb932083
vspaz <vladimir.edelman@gmail.com>
2021-11-09 14:43:04 +0100

    improve tests

--- concurrency/select_test.go
+++ concurrency/select_test.go
@@ -8,7 +8,8 @@ import (
 )
 
 var (
-	waitInterval = time.Second * 5
+	waitIntervalForWorker_1 = time.Second * 5
+	waitIntervalForWorker_2 = time.Millisecond * 100
 )
 
 func worker(id int, channel chan string, waitInterval time.Duration) {
@@ -23,13 +24,13 @@ func TestGoroutineBlocked(t *testing.T) {
 	channel_2 := make(chan string)
 
 	go worker(1, channel_1, time.Millisecond*100)
-	go worker(2, channel_2, waitInterval)
+	go worker(2, channel_2, waitIntervalForWorker_2)
 
 	for i := 0; i < 5; i++ {
 		start := time.Now()
 		assert.Equal(t, fmt.Sprintf("worker 2; message '%d'", i), <-channel_2)
 		end := time.Now()
-		assert.GreaterOrEqual(t, waitInterval, end.Second()-start.Second()) // blocked for waitInterval seconds.
+		assert.GreaterOrEqual(t, waitIntervalForWorker_2, time.Duration(end.Second()-start.Second())) // blocked for waitInterval seconds.
 		assert.Equal(t, fmt.Sprintf("worker 1; message '%d'", i), <-channel_1)
 	}
 }
@@ -38,16 +39,20 @@ func TestGoroutineNonBlocking(t *testing.T) {
 	channel_1 := make(chan string)
 	channel_2 := make(chan string)
 
-	go worker(1, channel_1, time.Millisecond)
-	go worker(2, channel_2, waitInterval)
+	go worker(1, channel_1, waitIntervalForWorker_1)
+	go worker(2, channel_2, waitIntervalForWorker_2)
 
+	var count int
 	for i := 0; i < 5; i++ {
 		start := time.Now()
 		select {
 		case _ = <-channel_2:
+			count++
 		case _ = <-channel_1:
 			end := time.Now()
-			assert.LessOrEqual(t, time.Duration(end.Second()-start.Second()), time.Second*1)  // non-blocking
+			assert.LessOrEqual(t, time.Duration(end.Second()-start.Second()), waitIntervalForWorker_1) // non-blocking
+			assert.Equal(t, i, count)
+			count++
 		}
 	}
 }
