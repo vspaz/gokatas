d0d4a594ae2435a08a19916df59020b2ad68dbd1
vspaz <vladimir.edelman@gmail.com>
2021-12-22 20:38:40 +0100

    add TestCountLinesOk

--- stringops/word_counter_test.go
+++ stringops/word_counter_test.go
@@ -10,3 +10,8 @@ func TestCountWordsOk(t *testing.T) {
 	testInput := bytes.NewBufferString("word1 word2 word3\n")
 	assert.Equal(t, 3, count(testInput, false))
 }
+
+func TestCountLinesOk(t *testing.T) {
+	testInput := bytes.NewBufferString("word1\n word2\n word3\n\n")
+	assert.Equal(t, 4, count(testInput, true))
+}
