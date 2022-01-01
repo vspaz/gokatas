e014b126b94cd33f339a94609a57cc99c5dbaa19
vspaz <vladimir.edelman@gmail.com>
2022-01-01 21:39:04 +0100

    add digitCount

--- math/digitcounter.go
+++ math/digitcounter.go
@@ -1,1 +1,12 @@
 package math
+
+func getDigitCount(num int) int {
+	var digitCount int
+	remainder := num
+
+	for remainder > 0 {
+		remainder = digitCount / 10
+		digitCount++
+	}
+	return digitCount
+}
