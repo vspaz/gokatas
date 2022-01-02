1ce9124114d6a38626cc43bc547f2ff223dd9fdf
vspaz <vladimir.edelman@gmail.com>
2022-01-02 15:42:34 +0100

    add 'TestReverseDigit'

--- math/reversedigit_test.go
+++ math/reversedigit_test.go
@@ -1,7 +1,10 @@
 package math
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestReverseDigit(t *testing.T) {
-
+	assert.Equal(t, 4321, ReverseDigit(1234))
 }
