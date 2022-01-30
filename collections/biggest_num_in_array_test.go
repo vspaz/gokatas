c5197e42f75fb599b2e4358ad2bc18e00b36b23c
vspaz <vladimir.edelman@gmail.com>
2022-01-30 20:00:00 +0100

    add more tests

--- collections/biggest_num_in_array_test.go
+++ collections/biggest_num_in_array_test.go
@@ -9,3 +9,11 @@ func TestGetBiggestNumInArray(t *testing.T) {
 	nums := []int{5, 3, 10, 2, -5, 8, 140, 80, 11}
 	assert.Equal(t, 140, getBiggestNumInArray(nums))
 }
+
+func TestEmptyArray(t *testing.T) {
+	assert.Panics(t, func() { getBiggestNumInArray([]int{}) })
+}
+
+func TestArrayOfSizeOne(t *testing.T) {
+	assert.Equal(t, 5, getBiggestNumInArray([]int{5}))
+}
